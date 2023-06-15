33695490a7c4b0f6798f5a21a4480bca04694788
Seezar75 <cesare.ali@gmail.com>
2023-06-15 22:22:35 +0200

    added keymaps

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -10,4 +10,23 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Better window navigation
+keymap("n", "<A-h>", "<C-w>h", opts)
+keymap("n", "<A-j>", "<C-w>j", opts)
+keymap("n", "<A-k>", "<C-w>k", opts)
+keymap("n", "<A-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<A-Up>", ":resize +2<CR>", opts)
+keymap("n", "<A-Down>", ":resize -2<CR>", opts)
+keymap("n", "<A-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<A-Right>", ":vertical resize +2<CR>", opts)
+
+-- move lines and hold on to yanked text
+keymap("v", "<C-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<C-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "p", '"_dP', opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
 keymap("i", "jk", "<ESC>", opts)
